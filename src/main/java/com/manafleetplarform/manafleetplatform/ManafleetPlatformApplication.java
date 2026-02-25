862700d31ac690204abf3b7f1f9b979f8d4c3dc8
DESKTOP-Q645JHO\manav <manavision@outlook.de>
2026-02-25 07:26:50 +0100

    Initial commit: Spring Boot Projekt Setup für ManaFleet

---
+++ src/main/java/com/manafleetplarform/manafleetplatform/ManafleetPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.manafleetplarform.manafleetplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManafleetPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManafleetPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manafleetplarform/manafleetplatform/ManafleetPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manafleetplarform.manafleetplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManafleetPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
